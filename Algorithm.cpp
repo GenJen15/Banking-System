c84f77ec05f83608fc32fb4e6a10103c7a210a2d
Fred Axzel Miano <genjen580@gmail.com>
2024-11-23 00:02:17 +0800

    Algorithm.cpp
    Still being developed...

---
+++ Algorithm.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main() {
+    
+    // Banking System 
+    
+    
+    // START
+    // ask username
+    // ask PIN
+    // EXIT
+    
+    /* if PIN EQUALS to username/pin then
+       CONTINUE
+       
+       // PRINT remaining currency
+       // ask user WITHDRAW or DEPOSIT
+    */
+    
+    /* IF withdraw then 
+       // ask user how many cash is needed to get withdrawn
+       // IF cash is sufficient then PROCEED to withdraw 
+       // ELSE LOOP back to ask user WITHDRAW or DEPOSIT
+    */
+    
+    /* IF deposit then 
+       // ask user how many cash is going to get DEPOSIT
+       // IF cash is lesser than 0 then LOOP back to ask user WITHDRAW or DEPOSIT
+       // ELSE continue to DEPOSIT
+    */
+    
+    /* LOOP back to ask user he want to do another transaction
+       // IF TRUE then LOOP backt ask user WITHDRAW or DEPOSIT
+       // ELSE END
+    */
+
+  return 0;
+}
